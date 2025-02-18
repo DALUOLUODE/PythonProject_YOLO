b7e2046b2a3621b5af4c7ab71e89b13d9222bac5
wangyu <2092998053@qq.com>
2025-02-18 18:32:55 +0800

    initial commit...6

--- Project2_PeopleCounter/People_Counter.py
+++ Project2_PeopleCounter/People_Counter.py
@@ -5,7 +5,7 @@ import cvzone
 import math
 import sort
 
-cap = cv2.VideoCapture('../Videos/cars.mp4')
+cap = cv2.VideoCapture('../Videos/people.mp4')
 
 model = YOLO('../Yolo_Weights/yolov8n.pt').to('cuda')
 
@@ -24,14 +24,16 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
 mask=cv2.imread('mask.png')
 # tracking
 tracker = sort.Sort(max_age= 20,min_hits= 3,iou_threshold= 0.3)
-limits=[400,297,673,297]
-totalCount=[]
+limitsUp=[103,161,296,161]
+limitsDown=[527,489,735,489]
+totalCountUp=[]
+totalCountDown=[]
 
 while True:
     success,frame=cap.read()
     frameRegion=cv2.bitwise_and(frame,mask)
     frameGraphics=cv2.imread('graphics.png',cv2.IMREAD_UNCHANGED)
-    frame=cvzone.overlayPNG(frame,frameGraphics,(0,0))
+    frame=cvzone.overlayPNG(frame,frameGraphics,(730,260))
     if not success:
         break
     results = model(frameRegion,stream=True,device='cuda')
@@ -45,8 +47,7 @@ while True:
             cls=int(box.cls[0])
             currentClass=classNames[cls]
 
-            if ((currentClass=="car" or currentClass=="truck" or currentClass=="motorbike"
-                    or currentClass=="bus") and conf>0.5):
+            if currentClass== "person" and conf>0.5:
                 # cvzone.putTextRect(frame,f'{currentClass} {conf}',(max(x1,0),max(y1+20,0)),
                 #                    scale=1,thickness=1,offset=6)
                 # cvzone.cornerRect(frame, (x1, y1, w, h), l=6,rt=5)
@@ -54,7 +55,10 @@ while True:
                 detections=sort.np.vstack((detections,currentArray))
 
     resultsTacker=tracker.update(detections)
-    cv2.line(frame,(limits[0],limits[1]),(limits[2],limits[3]),(0,0,255),5)
+
+    cv2.line(frame,(limitsUp[0],limitsUp[1]),(limitsUp[2],limitsUp[3]),(0,0,255),5)
+    cv2.line(frame,(limitsDown[0],limitsDown[1]),(limitsDown[2],limitsDown[3]),(0,0,255),5)
+
     for result in resultsTacker:
         x1,y1,x2,y2,id=result
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
@@ -65,13 +69,22 @@ while True:
 
         cx,cy=x1+w//2,y1+h//2
         cv2.circle(frame,(cx,cy),2,(255,0,255),2,cv2.FILLED)
-        if limits[0]<cx<limits[2] and limits[1]-20<cy<limits[1]+20:
-            if totalCount.count(id)==0:
-                totalCount.append(id)
-                cv2.line(frame, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
+
+        if limitsUp[0]<cx<limitsUp[2] and limitsUp[1]-20<cy<limitsUp[1]+20:
+            if totalCountUp.count(id)==0:
+                totalCountUp.append(id)
+                cv2.line(frame, (limitsUp[0], limitsUp[1]), (limitsUp[2], limitsUp[3]), (0, 255, 0), 5)
+
+        if limitsDown[0]<cx<limitsDown[2] and limitsDown[1]-20<cy<limitsDown[1]+20:
+            if totalCountDown.count(id)==0:
+                totalCountDown.append(id)
+                cv2.line(frame, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 255, 0), 5)
 
     # cvzone.putTextRect(frame, f'Count:{len(totalCount)}', (50,50),)
-    cv2.putText(frame,str(len(totalCount)),(255,100),cv2.FONT_HERSHEY_PLAIN,5,(50,50,255),3)
+    cv2.putText(frame,str(len(totalCountUp)),(929,345),cv2.FONT_HERSHEY_PLAIN,5,(139,195,75),7)
+    cv2.putText(frame,str(len(totalCountDown)),(1191,345),cv2.FONT_HERSHEY_PLAIN,5,(50,50,230),7)
+
+
     cv2.imshow("frame",frame)
     # cv2.imshow("frameRegion",frameRegion)
     if cv2.waitKey(1) & 0xFF == ord('q'):
